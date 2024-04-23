09ae17ea1207361378258d73b5f7622146048b41
Ganesh-Tambe <145638397+Ganesh-Tambe@users.noreply.github.com>
2024-04-23 05:35:39 +0000

    Comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,12 @@
+ console.log("02">1)
+ console.log("02"<1)
+
+ console.log("---------------")
+ console.log(null>0)
+ console.log(null==0)
+ console.log(null>=0)
+
+ console.log("---------------")
+ console.log(undefined==0)
+ console.log(undefined>0)
+ console.log(undefined>=0)
